5017e2da871569605cdd198ba1a3853fdfe3caca
Michael <atki7828@users.noreply.github.com>
2020-04-07 16:53:53 -0700

    parallax is working now

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    Camera cam;
+    GameObject player;
+    List<GameObject> layers;
+    float speed = 10f;
+    float playerPos;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        Debug.Log(player);
+        playerPos = player.transform.position.x;
+        cam = Camera.main;
+        layers = new List<GameObject>();
+
+        foreach(Transform child in transform)
+        {
+            layers.Add(child.gameObject);
+            AddNewSprite(child.gameObject);
+        }
+
+        foreach(GameObject l in layers)
+        {
+            l.transform.parent = this.transform;
+        }
+    }
+
+    private void AddNewSprite(GameObject go)
+    {
+        SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+        float width = sr.sprite.bounds.size.x;
+
+        GameObject newLayer = new GameObject();
+        newLayer.AddComponent<SpriteRenderer>().sprite = sr.sprite;
+        newLayer.GetComponent<SpriteRenderer>().sortingOrder = sr.sortingOrder;
+        newLayer.transform.position += new Vector3(width, 0, 0);
+        //newLayer.transform.parent = this.transform;
+        layers.Add(newLayer);
+
+        newLayer = new GameObject();
+        newLayer.AddComponent<SpriteRenderer>().sprite = sr.sprite;
+        newLayer.GetComponent<SpriteRenderer>().sortingOrder = sr.sortingOrder;
+        newLayer.transform.position -= new Vector3(width, 0, 0);
+        //newLayer.transform.parent = this.transform;
+        layers.Add(newLayer);
+    }
+
+    void moveTo(float x)
+    {
+        foreach(GameObject l in layers)
+        {
+            float dist = -(layers.Count + l.GetComponent<SpriteRenderer>().sortingOrder*speed);
+            l.transform.position += new Vector3(x * dist * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, cam.transform.position.y, this.transform.position.z);
+        moveTo(player.transform.position.x - playerPos);
+        playerPos = player.transform.position.x;
+    }
+
+}
+
---
+++ Assets/Scripts/CreateGround.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+[RequireComponent(typeof(TilemapCollider2D))]
+public class CreateGround : MonoBehaviour
+{
+    Tilemap map;
+    [SerializeField]
+    Sprite blockSprite;
+    Tile block;
+    int length = 200;
+
+    void Start()
+    {
+        map = this.GetComponent<Tilemap>();
+        block = new Tile();
+        block.sprite = blockSprite;
+        for(int i = -length; i < length; i++)
+        {
+            map.SetTile(new Vector3Int(i, -3, 0), block);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
